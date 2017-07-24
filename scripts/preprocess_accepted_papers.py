e331bc150fcec4f60662970caec382bad585c3c6
Anders Johannsen <anders@johannsen.com>
2017-07-24 20:09:58 +0100

    change author order

--- scripts/preprocess_accepted_papers.py
+++ scripts/preprocess_accepted_papers.py
@@ -5,12 +5,12 @@ def rename_column(old_name):
     return str(old_name).lower().replace(" ", "_")
 
 def first_author_last_name(author_names):
-    first_author = re.split("(,|and)", author_names)[0]
+    first_author = re.split(r"(,|\band\b)", author_names)[0]
     last_name = first_author.strip().split(" ")[-1].lower()
     return last_name
 
 
-D = pd.read_excel("accepted_papers.xsl")
+D = pd.read_excel("accepted_papers.xls")
 D = D.rename_axis(rename_column, axis='columns')
 D['first_author_last_name'] = D.authors.map(first_author_last_name)
 D['submission_type'] = D.submission_type.str.split().str[0]
